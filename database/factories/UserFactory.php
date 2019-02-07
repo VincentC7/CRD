927b6dc0881d4655f701567b6705c93dbba7cfcb
Killian BALLAND <killian.balland@outlook.fr>
2019-02-07 10:57:08 +0100

    Ajout du booleen pour savoir si l'utilisateur est un administrateur ou non

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,6 +18,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->unique()->name,
         'email' => $faker->unique()->safeEmail,
         'description' => $faker->description,
+        'admin' => $faker->default(false)->admin,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
