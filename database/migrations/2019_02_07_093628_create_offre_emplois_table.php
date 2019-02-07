990fcee7af2c7e35824fcfed73ea9302bfbcfcfd
Vanessa Geny <geny17@etu.univ-lorraine.fr>
2019-02-07 17:27:05 +0100

    Fix bug migration db

--- database/migrations/2019_02_07_093628_create_offre_emplois_table.php
+++ database/migrations/2019_02_07_093628_create_offre_emplois_table.php
@@ -21,7 +21,6 @@ class CreateOffreEmploisTable extends Migration
             $table->text('durée');
             $table->string('lieu_travail');
             $table->boolean('actif')->default(0);
-            $table->unsignedInteger('id_categorie')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
