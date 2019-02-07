59f2cded1420bfee72dc41bca54f8933a510aa85
CHOQUERT Vincent <vincent.choquert4@etu.univ-lorraine.fr>
2019-02-07 11:47:00 +0100

    offre emplois control

--- app/Http/Controllers/OffreEmploisController.php
+++ app/Http/Controllers/OffreEmploisController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\OffreEmplois;
 use Illuminate\Http\Request;
 
-class OffreEmploisController extends Controller
-{
-    //
+class OffreEmploisController extends Controller {
+
+    public function index() {
+        //return view('projects.index', ['projects'=>Projects::all()]);
+    }
+
+    public function create() {
+        return view('newOffreEmplois');
+    }
+
+    public function store() {
+        /**
+         *  request()->validate([
+        'title'=> ['required', 'min:5', 'max:255'],
+        'description'=> ['required', 'min:5'],
+        ]);
+        Projects::create(request(['title','description']));
+        return redirect('/projects');
+         */
+    }
+
+    public function show(OffreEmplois $offfre) {
+        //return view('projects.show', compact('project'));
+    }
+
+    public function edit(OffreEmplois $offre){
+        //return view('projects.edit', compact('project'));
+    }
+
+    public function update(OffreEmplois $offre) {
+        /**
+         * $attribut = request()->validate([
+        'title'=> 'required',
+        'description'=> 'required',
+        ]);
+        $project->update($attribut);
+        return redirect('/projects');
+         */
+
+    }
+
+    public function destroy(OffreEmplois $offre) {
+        /**
+         * $project->delete();
+        return redirect('/projects');
+         */
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('OffreEmplois', 'OffreEmploisController');
 Route::get('/profil', function(){
     return view ('profil');
 });
