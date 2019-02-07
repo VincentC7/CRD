21074762225c8d435aee79c1770a5380866ab8be
CHOQUERT Vincent <vincent.choquert4@etu.univ-lorraine.fr>
2019-02-07 10:10:21 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
