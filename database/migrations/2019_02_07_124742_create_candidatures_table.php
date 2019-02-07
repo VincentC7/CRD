c1d3c6d3cee735e3266a9c1881d7619217dec4dc
CHOQUERT Vincent <vincent.choquert4@etu.univ-lorraine.fr>
2019-02-07 14:09:45 +0100

    MiddleWare creation offre emplois + creation Candidature

---
+++ database/migrations/2019_02_07_124742_create_candidatures_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCandidaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidatures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('id_offre');
+            $table->unsignedInteger('id_candidat');
+            $table->string('etat')->default('En attente');
+            $table->string('lieuDep')->default(null);
+            $table->boolean('transport');
+            $table->string('typeVehicule');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidatures');
+    }
+}
