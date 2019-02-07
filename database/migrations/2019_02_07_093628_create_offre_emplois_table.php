d304659a52de12fbd5b0850a9210af5f8b7fd412
Killian BALLAND <killian.balland@outlook.fr>
2019-02-07 11:01:26 +0100

    Ajout de la clé étrangère pour l'offre d'emplois

--- database/migrations/2019_02_07_093628_create_offre_emplois_table.php
+++ database/migrations/2019_02_07_093628_create_offre_emplois_table.php
@@ -15,7 +15,7 @@ class CreateOffreEmploisTable extends Migration
     {
         Schema::create('offre_emplois', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('id_employer');
+            $table->unsignedInteger('id_employer')->references('id')->on('users')->onDelete('cascade');
             $table->text('profil');
             $table->text('durée');
             $table->string('lieu_travail');
